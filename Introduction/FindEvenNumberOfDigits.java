59144c3ab1a21fe104a63d09989456522d43010a
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-08-25 22:28:53 +0200

    Update FindEvenNumberOfDigits.java
    Code for finding an even number of digits inside an array

--- Introduction/FindEvenNumberOfDigits.java
+++ Introduction/FindEvenNumberOfDigits.java
@@ -1,3 +1,20 @@
-public class FindEvenNumberOfDigits {
-    
+// Given an array nums of integers, 
+// return how many of them contain an even number of digits.
+
+class FindEvenNumberOfDigits {
+    public int findNumbers(int[] nums) {
+        // Initialize variable for counter
+        int evenDigitsCount = 0;
+        // Start looping through numbers of array
+        for( int num : nums) {
+            // Transform numbers into string and count its characters
+            String s = String.valueOf(num);
+            // If length of string is even then counter goes up
+            if(s.length() % 2 == 0) {
+                evenDigitsCount++;
+            }
+        }
+        //Return counter
+        return evenDigitsCount;
+    } 
 }
