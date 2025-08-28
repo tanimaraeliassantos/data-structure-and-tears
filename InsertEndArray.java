1409991772491a07a15573cc650cc06a7745d511
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-08-28 22:10:09 +0200

    Create InsertEndArray.java
    Created new array with length of 6
    Created class's constructor
    Added for loop to run through 3 elements to be added to end of array

---
+++ InsertEndArray.java
@@ -0,0 +1,12 @@
+public class InsertEndArray {
+    int[] intArray = new int[6];
+    int length = 0;
+
+    public InsertEndArray() {
+        for (int i = 0; i < 3; i++) {
+            intArray[length] = i;
+            length++;
+        }
+    }
+
+}
