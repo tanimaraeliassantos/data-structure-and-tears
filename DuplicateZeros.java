359f3c9293dc983f6724e88c64983033d26bfefc
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-09-09 13:26:32 +0200

    Update DuplicateZeros.java

--- DuplicateZeros.java
+++ DuplicateZeros.java
@@ -2,5 +2,44 @@
 //  of zero, shifting the remaining elements to the right.
 
 public class DuplicateZeros {
-  int[] arr = new int[6];
+    public void duplicateZerosArray(int[] array) {
+        // Count length of array
+        int n = array.length;
+        // Initialize array with fixed length
+        int[] resultingArray = new int[n];
+        // Initialize pointer for original array
+        int i = 0;
+        // Initialize pointer for new array
+        int possible_dups = 0;
+
+        // Loop through i of array
+        while (i < n && possible_dups < n) {
+            // if the i is 0
+            if (array[i] == 0) {
+                // then we have to set the pointer to the new array as zero
+                resultingArray[possible_dups] = 0;
+                // then we have to increment the new array by one element
+                possible_dups++;
+                // if the new array still has the length of the original array
+                if (possible_dups < n) {
+                    // then we duplicate the zero
+                    resultingArray[possible_dups] = 0;
+                }
+
+            } else {
+                // if not zero copy element to new array
+                resultingArray[possible_dups] = array[i];
+            }
+            // after looping through i
+            // increment pointer and move to next element of the original array
+            i++;
+            // increment pointer and move to next element of new array
+            possible_dups++;
+        }
+
+        // copy new into original array
+        for (int k = 0; k < n; k++) {
+            array[k] = resultingArray[k];
+        }
+    }
 }
