dd4a8af2ec8130f76afc7af3f07db6ce5161afc9
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-09-09 12:54:57 +0200

    Update DuplicateZeros.java
    Created array of length 6

--- DuplicateZeros.java
+++ DuplicateZeros.java
@@ -1,3 +1,6 @@
+// Given a fixed-length integer array arr, duplicate each occurrence
+//  of zero, shifting the remaining elements to the right.
+
 public class DuplicateZeros {
-    
+  int[] arr = new int[6];
 }
