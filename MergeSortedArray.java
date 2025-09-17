478ab037da8ec18af09a70b497d26f9a7a824b73
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-09-17 14:13:14 +0200

    Update MergeSortedArray.java

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /*
  * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
  * and two integers m and n, representing the number of elements
@@ -11,5 +14,22 @@
  * n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
 public class MergeSortedArray {
-    
+    /*
+     * Declare method, define input parameters: an integer nums1, the number of valid
+     * elements in nums1, the integer array nums2 (to be merged into nums1),
+     * the number of elements in nums2
+     */
+   
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //Start for loop with counter in 0 and let it continue for as long as the counter
+        // is less than number of elements in nums2
+        for (int i = 0; i < n; i++) {
+            //Access element at index i in nums2 array, calculate the correct position
+            // in nums1 where the element from nums2 should go
+            nums1[m + i] = nums2[i];
+        }
+        //sort the array in non-decreasing order
+        Arrays.sort(nums1);
+    }
 }
+ß
