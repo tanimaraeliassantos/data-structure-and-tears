adc18f7307207042655431d88c08bc5f1bd63e86
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-07-08 13:28:06 +0200

    Update CheckIfExist.java

--- CheckIfExist.java
+++ CheckIfExist.java
@@ -5,16 +5,16 @@ public class CheckIfExist {
         // Crear lista para guardar los números que ya fueron vistos
         HashSet<Integer> seen = new HashSet<>();
         // Loop cada número del array
-        for (int num: array) {
+        for (int num : array) {
             // si vimos que el array contiene el doble o si contiene la mitad del número
-            if (seen.contains(num * 2) || (num % 2 == 0 && seen.contains(num /2))) {
-                //devolver true
+            if (seen.contains(num * 2) || (num % 2 == 0 && seen.contains(num / 2))) {
+                // devolver true
                 return true;
             }
-            //si no hay match todavía, añadir número a la lista de números
+            // si no hay match todavía, añadir número a la lista de números
             seen.add(num);
         }
-        //si checkeamos el array y no encontramos nada, devolver false.
+        // si checkeamos el array y no encontramos nada, devolver false.
         return false;
     }
 }
