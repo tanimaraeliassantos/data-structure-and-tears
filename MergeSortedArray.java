4cf7b398bee028b11e091d4886c409f3a52eb0be
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-07-23 21:47:24 +0200

    Update MergeSortedArray.java

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,6 +1,4 @@
 
-import java.util.Arrays;
-
 /*
  * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
  * and two integers m and n, representing the number of elements
