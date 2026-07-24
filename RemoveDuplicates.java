b317ad49d31bdcb2d429cff4caeeda9fce80aee6
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-07-24 19:53:32 +0200

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -2,9 +2,9 @@ public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums) {
         //si el tamaño de nums es igual a 0, devolver 0
         if (nums.length == 0) return 0;
-        //inicializar i en 0
+        //inicializar writer en 1, que es el índice del siguiente elemento único
         int writer = 1;
-        //Recorrer el array j, empezando desde 1, si j es menor que el tamaño del array nums, sumamos un elemento al array
+        //Recorrer el array nums, empezando desde 1, si reader es menor que el tamaño del array nums, sumamos un elemento al array
         for (int reader = 1; reader < nums.length; reader++) {
             //si el numero actual es diferente del numero de antes
             if (nums[reader] != nums[reader-1]) {
